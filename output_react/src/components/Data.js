279b8c1ca044b08bd35b0255450a6bf269cfab58
Ivy Fan <ivyfan@Ivys-MacBook-Pro.local>
2020-11-26 11:06:39 -0500

    update CAS, visualized toy data

--- output_react/src/components/Data.js
+++ output_react/src/components/Data.js
@@ -68,14 +68,135 @@ class Data extends React.Component {
      * For now, following a scheme like this: https://bl.ocks.org/ctufts/f38ef0187f98c537d791d24fda4a6ef9 
      */
     buildVisualization(data) {
-        console.log(data);
+        // transform data into a list of nodes and directed edges (from parent to child)
+        const fakeData = {
+            key: 'everything',
+            value: [
+            {
+                key: 'topic1',
+                value: [
+                    {name: 'club1top1', key: 'topic1'},
+                    {
+                        key: 'subtopic2',
+                        value: [
+                            {name: 'club2top1subtop2', key: 'subtopic2'},
+                            {name: 'club5top1subtop2', key: 'subtopic2'},
+                            {name: 'club6top1subtop2', key: 'subtopic2'},
+                            {name: 'club7top1subtop2', key: 'subtopic2'}
+                        ]
+                    },
+                    {name: 'club3top1', key: 'topic1'},
+                    {name: 'club4top1', key: 'topic1'},
+                ] 
+            },
+            {
+                key: 'topic2',
+                value: [
+                    {name: 'club1top2'},
+                    {
+                        key: 'subtopic2',
+                        value: [
+                            {name: 'club2top2subtop2'},
+                            {name: 'club5top2subtop2'},
+                            {name: 'club6top2subtop2'},
+                            {name: 'club7top2subtop2'}
+                        ]
+                    },
+                    {name: 'club3top2'},
+                    {name: 'club4top2'},
+                ] 
+            }
+        ]}
 
-        // within each cluster, 
+        let nodeCount = 0;
+        // TODO: these functions
+        function nodesAndEdges(tree, nodeList, edgeList, parentID) {
+            // if leaf, append self to nodelist
+            if ('name' in tree) {
+                const leaf = {...tree, id: nodeCount};
+                nodeCount++;
+                nodeList.push(leaf);
+                edgeList.push({source: parentID, target: leaf.id});
+            } else { // append self to nodelist, call getNodes on each child
+                const node = {...tree, id: nodeCount};
+                nodeCount++;
+                nodeList.push(node);
+                if(parentID != undefined) {
+                    edgeList.push({source: parentID, target: node.id});
+                }
+                tree.value.forEach((child) => {
+                    console.log(child)
+                    nodesAndEdges(child, nodeList, edgeList, node.id);
+                })
+            }
+        }
 
-        // for names, append them over the top...
-        d3.select(this._rootNode)
-            .append('div')
-            .text('hello, world')
+        let nodeList = [];
+        let edgeList = [];
+        nodesAndEdges(fakeData, nodeList, edgeList, undefined);
+        console.log(nodeList);
+        console.log(edgeList);
+
+        // create constrained force layout with nodes and edges
+        // set up workspace
+        const margin = {
+            top: 60,
+            bottom: 60,
+            right: 60,
+            left: 60,
+        };
+        
+        const height = 700 - margin.top - margin.bottom;
+        const width = 700 - margin.top - margin.bottom;
+
+        const forcePlot = d3.select(this._rootNode)
+            .append('svg')
+            .attr('width', width + margin.left + margin.right)
+            .attr('height', height + margin.top + margin.bottom)
+            .append('g')
+            .attr('transform', `translate(${margin.left}, ${margin.top})`);
+        
+        const force = d3.forceSimulation(nodeList)
+            .force('charge', d3.forceManyBody()
+              .strength(-70))
+            .force('link', d3.forceLink(edgeList)
+              .distance(150))
+            .force('center', d3.forceCenter().x(width / 2).y(height / 2));
+        
+        const edges = forcePlot.selectAll('line')
+            .data(edgeList)
+            .enter()
+            .append('line')
+            .style('stroke', 'black')
+            .attr('stroke-opacity', 0.1)
+            .style('stroke-width', 1);
+      
+        const nodes = forcePlot.selectAll('circle')
+            .data(nodeList)
+            .enter()
+            .append('circle')
+            .attr('r', 6)
+            .attr('fill', 'blue');
+
+        const labels = forcePlot.selectAll('text')
+            .data(nodeList)
+            .enter()
+            .append('text')
+            .attr('font-family', 'sans-serif')
+            .attr('font-size', 'small')
+            .text((d) => d.name)
+            .style('pointer-events', 'none');
+
+        force.on('tick', () => {
+            edges.attr('x1', (d) => d.source.x)
+                .attr('y1', (d) => d.source.y)
+                .attr('y2', (d) => d.target.y)
+                .attr('x2', (d) => d.target.x);
+            nodes.attr('cx', (d) => d.x)
+                .attr('cy', (d) => d.y);
+            labels.attr('x', (d) => d.x + 4)
+                .attr('y', (d) => d.y - 4);
+        });
     }
 
     shouldComponentUpdate() {
@@ -89,22 +210,6 @@ class Data extends React.Component {
     render() {
         return <div className="line-container" ref={this._setRef.bind(this)} />
     }
-
-    // render() {
-    //     const orgs = this.state.organizations;
-    //     return(
-    //         <div>
-    //             {orgs.map(org => (
-    //                 <div key={org.name}>
-    //                     <p> {org.name} </p>
-    //                     {org.tags.map(tag => (
-    //                         <Chip key={Math.random()} label={tag} color='secondary' />
-    //                     ))}
-    //                 </div>
-    //             ))}
-    //         </div>
-    //     )
-    // }
 }
 
 export default Data;
