101d8c31090944bcd880c6277d0b0614af83909e
Tom Coile <tomcoile@Toms-MacBook-Pro.local>
2020-11-28 19:10:22 -0500

    zoom implemented

--- output_react/src/components/Data.js
+++ output_react/src/components/Data.js
@@ -3,24 +3,21 @@ import * as d3 from "d3";
 import cloneDeep from "lodash/cloneDeep";
 import '../App.css';
 import './Data.css'
+import { linkVertical } from "d3";
 // import Chip from '@material-ui/core/Chip';
 
 class Data extends React.Component {
 
     async componentDidMount() {
-        console.log("component mounting"); // looks like this happens twice - why?
         const data = await this.getData();
-        // console.log(data);
         const parsedData = this.parseData(data);
         this.buildVisualization(parsedData);
     }
 
     async getData() {
-        console.log("getting response");
         let response = await fetch('/get_organizations', {
             mode: 'cors'
         }).catch(() => console.log("could not fetch organizations"));
-        console.log(response);
         let jsonRes = await response.json().catch(() => console.log("not a valid json thing"));
         return jsonRes;
     }
@@ -40,7 +37,6 @@ class Data extends React.Component {
         let map = {};
         // Further Nesting is possible by recursing on these methods
         let newThing = this.parseDataHelper(data, [], true, map);
-        console.log(map);
         return newThing;
     }
 
@@ -76,8 +72,6 @@ class Data extends React.Component {
         // Collapse: d3.group, then flatten into an array
         const groupedData = d3.group(keyedData, (d) => d.key);
         const groupDataArray = Array.from(groupedData, ([key, value]) => ({ name: key, children: value }));
-        // console.log(groupedData);
-        // console.log(groupDataArray);
 
         let recursiveThreshold = 5;
         this.mergeSupersets(groupDataArray, topLevel, supersetMap);
@@ -254,42 +248,80 @@ class Data extends React.Component {
     }
 
     /**
-     * We're building this visualization in the MOST d3 way, 
+     * Visualization! We're building this visualization in the MOST d3 way, 
      * the LEAST React way possible
      */
     buildVisualization(data) {
+        /**
+         * Get nodes and links
+         */
+    
         const root = d3.hierarchy({name: 'everything', children: data});
 
-        // Returns a list of all nodes under the root.
+        // Adds an identity field to each node - useful for data joins
+        // Also adds a cluster ID to each node - useful for blobs
         function flatten(root) {
-            var nodes = [], i = 0;
+            let clusters = [], i = 0, cluster = 0;
         
             function recurse(node) {
                 if (node.children) {
                     node.children.forEach(recurse);
-                    node._children = node.children;
-                    node.children = null;
                 }
                 if (!node.identity) node.identity = ++i;
-                nodes.push(node);
+            }
+
+            function recurseCluster(node) {
+                if(node.children) {
+                    node.children.forEach(recurseCluster);
+                }
+                node.cluster = cluster;
+                if(clusters[cluster] === undefined) {
+                    clusters[cluster] = [];
+                }
+                clusters[cluster].push(node);
             }
         
             recurse(root);
+            root.children.forEach((child) => {
+                recurseCluster(child);
+                cluster++;
+            })
+            return clusters;
+        }
+
+        const clusters = flatten(root)
+        console.log(clusters);
+
+        function getLinks() { // remove links from root to all children
+            const links = root.links();
+            // if link.source.identity == root.identity, delete this link
+            while(links[0].source.identity === root.identity) {
+                links.shift();
+            }
+            return links;
+        }
+
+        function getNodes() { // remove everything node
+            const nodes = root.descendants();
+            nodes.shift();
             return nodes;
         }
 
-        flatten(root)
-        let links = root.links();
-        let nodes = root.descendants();
+        let links = getLinks();
+        let nodes = getNodes();
 
+        /**
+         * Create force simulation, color schemes, and overall svg
+         */
         const simulation = d3.forceSimulation(nodes)
-            .force("link", d3.forceLink(links).id(d => d.id).distance(30).strength(0.5))
-            .force("charge", d3.forceManyBody().strength(-100))
+            .force("link", d3.forceLink(links).id(d => d.identity)
+                .distance((d) => (d.target.children || d.target._children) ? 20 : 10)
+                .strength(1.6))
+            .force("charge", d3.forceManyBody().strength((d) => {
+                return d.children || d._children ? -200 : -60;
+            }))
             .force("x", d3.forceX())
             .force("y", d3.forceY())
-
-        const width = 1000;
-        const height = 490;
 
         const color = d3.scaleOrdinal()
             .domain(data, (d) => d.name) 
@@ -297,13 +329,17 @@ class Data extends React.Component {
 
         // Enable zooming, panning
         function zoomed({transform}) {
+            // console.log(transform)
             forcePlot.attr("transform", transform);
         }
 
         const zoom = d3.zoom()
-            .scaleExtent([1, 20])
+            .scaleExtent([0.2, 10])
             .on("zoom", zoomed);
 
+        const width = 1000;
+        const height = 490;
+    
         const forceSvg = d3.select(this._rootNode)
             .append('svg')
             .attr('id', 'force-svg')
@@ -313,15 +349,11 @@ class Data extends React.Component {
         
         const forcePlot = forceSvg.append('g')
             
+        /**
+         * Interactivity
+         */
         
-        let link = forcePlot.append('g')
-                .attr('stroke', '#999')
-                .attr('stroke-opacity', 0.6)
-            .selectAll('line')
-            .data(links, (d) => [d.source, d.target])
-            .join('line');
-        
-        // add interactivity
+        // drag interactions
         const drag = simulation => {
             function dragstarted(event, d) {
                 if (!event.active) simulation.alphaTarget(0.3).restart();
@@ -346,58 +378,45 @@ class Data extends React.Component {
                 .on("end", dragended);
         }
 
-        // Add click interactivity
-        function click(event, d) {
+        // Click interactions
+        async function click(event, d) {
             if (event.defaultPrevented) return; // ignore drag
+            // handles tag actions
             if (d.children) { // make them disappear
               d._children = d.children;
               d.children = null;
             } else if (d._children) { // make them appear, if not a leaf
+              centerElement(event, this, 4);
               d.children = d._children;
               d.children.map((node) => { // make children appear in the right place
                   let delta_x = 0;
                   let slope = 1;
-                  if (node.parent !== null && node.parent.parent != null) { // have a grandparent?
+                  if (node.parent !== null && node.parent.parent !== null && node.parent.parent.identity !== root.identity) { // have a grandparent?
                     // travel a bit in the rough direction of the line our parent/grandparent make
                     delta_x = node.parent.x - node.parent.parent.x;
                     const delta_y = node.parent.y - node.parent.parent.y;
                     slope = delta_y / delta_x;
                   }
-
-                  node.x = node.parent.x + delta_x * 3/4 + Math.random() * 15;
-                  node.y = node.parent.y + slope * delta_x * 3/4 + Math.random() * 15;
+                  node.x = node.parent.x + delta_x * 3/4 + Math.random() * 10;
+                  node.y = node.parent.y + slope * delta_x * 3/4 + Math.random() * 10;
                   return node;
               })
               d._children = null;
+            } else { // handles leaf actions
+                positionLeaf(event, this);
+                // TODO: Call the drawer to scroll out, with d's information
+                console.log(d);
             }
-            update();
+            
+        
+            update(); // update forces some data joins, then restarts the vis
         }
 
-        /**
-         * Handle mouseover interactions
-         */
-        function handleMouseOver(event, node) {
-            d3.select(this).transition()
-                .delay(30)
-                .duration(200)
-                .attr('r', (d) => d.children || d._children ? 9 : 8);
-        }
-
-        function handleMouseOut(event, node) {
-            d3.select(this).transition()
-                .delay(30)
-                .duration(200)
-                .attr('r', (d) => d.children || d._children ? 7 : 6);
-        }
-
-        /**
-         * This function looks deceptively simple, but took me 4 hours to create. 
-         * I still do not know why I can't do enter() and exit() sequences as opposed to join - 
-         * perhaps because of the way tick works? 
-         */
+        // update nodes, links, then restart visualization
         function update() {
-            nodes = root.descendants();
-            links = root.links();
+
+            nodes = getNodes();
+            links = getLinks();
 
             link = link.data(links, d => [d.source, d.target])
                 .join('line');
@@ -421,6 +440,219 @@ class Data extends React.Component {
             simulation.restart();
         } 
 
+        // hover interactions
+        function handleMouseOver(event, node) {
+            d3.select(this).transition()
+                .duration(150)
+                .attr('r', (d) => d.children || d._children ? 10 : 8);
+        }
+
+        function handleMouseOut(event, node) {
+            d3.select(this).transition()
+                .delay(30)
+                .duration(200)
+                .attr('r', (d) => d.children || d._children ? 7 : 6);
+        }
+
+        // BLOB ME UP
+        const groupPath = function(d) {
+            const hull = d3.polygonHull(d.map((node) => [node.x, node.y]))
+            return hull;
+        };
+
+        function getCentroid(element) {
+            const bbox = element.getBBox();
+            return [bbox.x + bbox.width/2, bbox.y + bbox.height/2];
+        }
+
+        async function positionLeaf(event, element) {
+            event.stopPropagation();
+            let x, y;
+            const center = getCentroid(element)
+            x = center[0] + width/2;
+            y = center[1];
+            await forceSvg.transition().duration(650).call(
+                zoom.translateTo,
+                x,
+                y).end()
+            
+        }
+
+        async function centerElement(event, element, k) {
+            event.stopPropagation();
+            let x, y;
+            
+            const center = getCentroid(element);
+            x = center[0];
+            y = center[1];
+              
+            await forceSvg.transition().duration(650).call(
+                zoom.translateTo,
+                x,
+                y).end();
+            await forceSvg.transition().duration(400).call(
+                zoom.scaleTo,
+                k
+            ).end();
+        }
+
+        function centerZoomBlob(event) {
+            centerElement(event, this, 1);
+        }
+
+        // tick interactions
+        simulation.on('tick', () => {
+            link.attr('x1', d => d.source.x)
+                .attr('y1', d => d.source.y)
+                .attr('x2', d => d.target.x)
+                .attr('y2', d => d.target.y);
+            node.attr('cx', d => d.x)
+                .attr('cy', d => d.y)
+            
+            convexHull.data(clusters)
+                .join('path')
+                .attr('d', (d) => {
+                    const points = groupPath(d);
+                    return smoothHull(points);
+                })
+                .attr('fill', (d, i) => color(data[i].name))
+                .attr('stroke', 'white')
+                .attr('stroke-width', 0.2)
+                .attr('stroke-linejoin', 'round')
+                .attr('opacity', 0.08)
+                .on('click', centerZoomBlob);
+
+            forceSvg.attr('viewBox', [-window.innerWidth/2, -window.innerHeight/2, window.innerWidth, window.innerHeight])
+        })
+
+        /**
+         * Functions that are magical, and which I do not fully understand
+         * but am using because I WANT THE PRETTY: 
+         * http://bl.ocks.org/hollasch/9d3c098022f5524220bd84aae7623478
+         */
+        const hullPadding = 40;
+
+        // Point/Vector Operations
+
+        const vecFrom = function (p0, p1) {               // Vector from p0 to p1
+            return [ p1[0] - p0[0], p1[1] - p0[1] ];
+        }
+
+        const vecScale = function (v, scale) {            // Vector v scaled by 'scale'
+            return [ scale * v[0], scale * v[1] ];
+        }
+
+        const vecSum = function (pv1, pv2) {              // The sum of two points/vectors
+            return [ pv1[0] + pv2[0], pv1[1] + pv2[1] ];
+        }
+
+        const vecUnit = function (v) {                    // Vector with direction of v and length 1
+            var norm = Math.sqrt (v[0]*v[0] + v[1]*v[1]);
+            return vecScale (v, 1/norm);
+        }
+
+        const vecScaleTo = function (v, length) {         // Vector with direction of v with specified length
+            return vecScale (vecUnit(v), length);
+        }
+
+        const unitNormal = function (pv0, p1) {           // Unit normal to vector pv0, or line segment from p0 to p1
+            if (p1 != null) pv0 = vecFrom (pv0, p1);
+            var normalVec = [ -pv0[1], pv0[0] ];
+            return vecUnit (normalVec);
+        };
+
+
+        // Hull Generators
+
+        const lineFn = d3.line()
+            .curve (d3.curveCatmullRomClosed)
+            .x (function(d) { return d.p[0]; })
+            .y (function(d) { return d.p[1]; });
+
+
+        const smoothHull = (polyPoints) => {
+            // Returns the SVG path data string representing the polygon, expanded and smoothed.
+            if(polyPoints === null) {
+                return;
+            }
+            const pointCount = polyPoints.length;
+
+            // Handle special cases
+            if (!polyPoints || pointCount < 1) return "";
+            if (pointCount === 1) return smoothHull1 (polyPoints);
+            if (pointCount === 2) return smoothHull2 (polyPoints);
+
+            const hullPoints = polyPoints.map (function (point, index) {
+                var pNext = polyPoints[(index + 1) % pointCount];
+                return {
+                    p: point,
+                    v: vecUnit (vecFrom (point, pNext))
+                };
+            });
+
+            // Compute the expanded hull points, and the nearest prior control point for each.
+            for (let i = 0;  i < hullPoints.length;  ++i) {
+                const priorIndex = (i > 0) ? (i-1) : (pointCount - 1);
+                const extensionVec = vecUnit (vecSum (hullPoints[priorIndex].v, vecScale (hullPoints[i].v, -1)));
+                hullPoints[i].p = vecSum (hullPoints[i].p, vecScale (extensionVec, hullPadding));
+            }
+
+            return lineFn (hullPoints);
+        }
+
+
+        const smoothHull1 = function (polyPoints) {
+            // Returns the path for a circular hull around a single point.
+
+            const p1 = [polyPoints[0][0], polyPoints[0][1] - hullPadding];
+            const p2 = [polyPoints[0][0], polyPoints[0][1] + hullPadding];
+
+            return 'M ' + p1
+                + ' A ' + [hullPadding, hullPadding, '0,0,0', p2].join(',')
+                + ' A ' + [hullPadding, hullPadding, '0,0,0', p1].join(',');
+        };
+
+
+        const smoothHull2 = function (polyPoints) {
+            // Returns the path for a rounded hull around two points.
+
+            const v = vecFrom (polyPoints[0], polyPoints[1]);
+            const extensionVec = vecScaleTo(v, hullPadding);
+
+            const extension0 = vecSum (polyPoints[0], vecScale(extensionVec, -1));
+            const extension1 = vecSum (polyPoints[1], extensionVec);
+
+            const tangentHalfLength = 1.2 * hullPadding;
+            const controlDelta    = vecScaleTo (unitNormal(v), tangentHalfLength);
+            const invControlDelta = vecScale (controlDelta, -1);
+
+            const control0 = vecSum (extension0, invControlDelta);
+            const control1 = vecSum (extension1, invControlDelta);
+            const control3 = vecSum (extension0, controlDelta);
+
+            return 'M ' + extension0
+                + ' C ' + [control0, control1, extension1].join(',')
+                + ' S ' + [          control3, extension0].join(',')
+                + ' Z';
+        };
+
+
+        /**
+         * Visualization initialization
+         */
+
+        let convexHull = forcePlot.selectAll('.blob')
+            .data(clusters)
+            .join('path')
+            .attr('class', 'hull');
+
+        let link = forcePlot.append('g')
+                .attr('stroke', '#999')
+                .attr('stroke-opacity', 0.6)
+            .selectAll('line')
+            .data(links, (d) => [d.source, d.target])
+            .join('line');
+
         let node = forcePlot.append('g')
                 .attr('fill', '#fff')
                 .attr('stroke', '#000')
@@ -440,16 +672,6 @@ class Data extends React.Component {
         node.append("title")
             .text(d => d.data.name);
 
-        // update on tick
-        simulation.on('tick', () => {
-            link.attr('x1', d => d.source.x)
-                .attr('y1', d => d.source.y)
-                .attr('x2', d => d.target.x)
-                .attr('y2', d => d.target.y);
-            node.attr('cx', d => d.x)
-                .attr('cy', d => d.y)
-            forceSvg.attr('viewBox', [-window.innerWidth/2, -window.innerHeight/2, window.innerWidth, window.innerHeight])
-        })
     }
 
     shouldComponentUpdate() {
